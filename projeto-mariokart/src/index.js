99b1115a112ef7eb4f8b9d5b73bbda1251d5ee30
LuizFelixDev <luizhenriquefelix138@gmail.com>
2025-07-13 16:59:16 -0300

    Solução provisória para a função logRollResult()

--- projeto-mariokart/src/index.js
+++ projeto-mariokart/src/index.js
@@ -37,34 +37,49 @@ async function getRandomBlock(){
     return result;
 }
 
+async function logRollResult(playerName, block, diceResult, atribute){
+    console.log(`${playerName} 🎲 Rolou um dado ${block} ${diceResult} + ${atribute} = ${diceResult + atribute}`);
+};
+
 async function playRaceEngine(character1, character2){
     for(let i=1; i<6; i++){
         console.log(`🏁 Rodada ${i}`);
         // Sortear bloco
         let block = await getRandomBlock();
         console.log(`Bloco: ${block}`);
+
+        let diceResult1 = await rollDice();    
+        let diceResult2 = await rollDice();
+
+        //Testar abilidade
+        let testSkill1 = 0;
+        let testSkill2 = 0;
+
+        if(block == "RETA"){
+            testSkill1 = rollDice + player1.VELOCIDADE;
+            testSkill2 = rollDice + player2.VELOCIDADE;
+
+            await logRollResult(player1.NOME, "velocidade", diceResult1, player1.VELOCIDADE);
+            await logRollResult(player2.NOME, "velocidade", diceResult2, player2.VELOCIDADE);
+        };
+        if(block == "CURVA"){
+            testSkill1 = rollDice + player1.MANOBRABILIDADE;
+            testSkill2 = rollDice + player2.MANOBRABILIDADE;
+
+            await logRollResult(player1.NOME, "manobrabilidade", diceResult1, player1.MANOBRABILIDADE);
+            await logRollResult(player2.NOME, "manobrabilidade", diceResult2, player2.MANOBRABILIDADE);
+        };
+        if(block == "CONFRONTO"){
+            testSkill1 = rollDice + player1.PODER;
+            testSkill2 = rollDice + player2.PODER;
+
+            await logRollResult(player1.NOME, "poder", diceResult1, player1.PODER);
+            await logRollResult(player2.NOME, "poder", diceResult2, player2.PODER);
+        };
     };
 
     //Rolar dado
-    let diceResult1 = await rollDice();    
-    let diceResult2 = await rollDice();
-
-    //Testar abilidade
-    let testSkill1 = 0;
-    let testSkill2 = 0;
-
-    if(block == "RETA"){
-        testSkill1 = rollDice + player1.VELOCIDADE;
-        testSkill2 = rollDice + player2.VELOCIDADE;
-    };
-    if(block == "CURVA"){
-        testSkill1 = rollDice + player1.MANOBRABILIDADE;
-        testSkill2 = rollDice + player2.MANOBRABILIDADE;
-    };
-    if(block == "CONFRONTO"){
-        testSkill1 = rollDice + player1.PODER;
-        testSkill2 = rollDice + player2.PODER;
-    };
+    
 };
 
 
