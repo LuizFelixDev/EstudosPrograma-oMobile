b6a0f517ef7330930fd2ae8acfc12584dea27c89
LuizFelixDev <luizhenriquefelix138@gmail.com>
2025-08-07 09:43:43 -0300

    Calculate total e Wishlist

--- 06-shopee-cart/src/services/cart.js
+++ 06-shopee-cart/src/services/cart.js
@@ -2,8 +2,8 @@
 
 //casos de uso
 // -> adicionar item no carrinho
-async function addItem(userCart){
-
+async function addItem(userCart, item){
+    userCart.push(item);
 }
 // -> deletar item do carrinho
 async function deleteItem(userCart, name) {
@@ -15,5 +15,13 @@ async function removeItem(userCart, index) {
 }
 // -> calcular o total
 async function calculateTotal(userCart) {
-    
+    const result = (userCart.reduce((total, item)=> total + item.subtotal(), 0));
+    console.log(result);
+}
+
+export {
+    addItem,
+    calculateTotal,
+    deleteItem,
+    removeItem,
 }
