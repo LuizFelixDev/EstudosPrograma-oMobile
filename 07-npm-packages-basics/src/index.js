53a4e4c74d9e257b101e6b8ad4fea10ee3a1b2eb
LuizFelixDev <luizhenriquefelix138@gmail.com>
2025-08-15 21:58:13 -0300

    Ultilizando dev dependencies

--- 07-npm-packages-basics/src/index.js
+++ 07-npm-packages-basics/src/index.js
@@ -1,4 +1,9 @@
 import chalk from 'chalk';
+import logSymbols from 'log-symbols';
 
-console.log(chalk.red.bgBlack.italic('Hello world!'));
-console.log(chalk.blue.bgBlack.italic('I`m Sonic X'));
+console.log(chalk.bgBlack.green(logSymbols.success,"Sercidor iniciado com sucesso!"));
+
+console.log(chalk.bgBlack.red(logSymbols.error, "Servidor com problema!"))
+
+// console.log(chalk.red.bgBlack.italic('Hello world!'));
+// console.log(chalk.blue.bgBlack.italic('I`m Sonic X'));
