63df3a00e74abc23869ecabf2102deabd550887e
LuizFelixDev <luizhenriquefelix138@gmail.com>
2025-09-10 17:25:06 -0300

    Utilizando tsup

---
+++ 10-typeScript/dist/index.cjs
@@ -0,0 +1,8 @@
+"use strict";
+
+// src/index.ts
+function main() {
+  let myEmail = "luiz.henri@email.com";
+  console.log(myEmail);
+}
+main();
--- 10-typeScript/dist/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-function main() {
-    let myEmail = "luiz.henrique@email.com";
-    console.log(myEmail);
-}
-main();
