4c4b1aa9581f3888fd7628b128512de06647f586
LuizFelixDev <luizhenriquefelix138@gmail.com>
2025-09-18 22:43:42 -0300

    Setup fast TS

--- 10-typeScript/dist/index.cjs
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-// src/index.ts
-function main() {
-  let myEmail = "luiz.henri@email.com";
-  console.log(myEmail);
-}
-main();
--- 10-typeScript/src/index.ts
+++
@@ -1,6 +0,0 @@
-function main(){
-    let myEmail:string = "luiz.henri@email.com";
-    console.log(myEmail);
-}
-
-main()
